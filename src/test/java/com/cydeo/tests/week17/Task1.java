a1d188fe765f9f501d21743b365519b9d0797246
virchenkoann <virchenko.ann@gmail.com>
2025-05-08 07:48:14 -0400

    initial project creation

---
+++ src/test/java/com/cydeo/tests/week17/Task1.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.week17;
+//```
+//
+//   TC001 As a user I should be able to login with valid credentials
+//
+//     1- Open a Chrome browser
+//     2- Go to "https://vytrack.com/"
+//     3- Click LOGIN button
+//     4- Login to application with username as "User1" and password as "UserUser123"
+//     5- Verify the title contains "Dashboard"
+//```
+public class Task1 {
+}
